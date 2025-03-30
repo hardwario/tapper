dba8b93e50b8c5a48918ed0dcf39f098aac9edbb
Martin <81148675+Martian-0007@users.noreply.github.com>
2025-03-30 12:13:56 +0200

    Fox logging location
    Signed-off-by: Martin <81148675+Martian-0007@users.noreply.github.com>

--- src/tapper/__init__.py
+++ src/tapper/__init__.py
@@ -5,9 +5,11 @@ from loguru import logger
 if not os.path.exists(os.path.join("~", ".tapper", "logs")):
     os.makedirs(os.path.join("~", ".tapper", "logs"), exist_ok=True)
 
+homedir = os.path.expanduser("~")
+
 logger.remove()
 logger.add(
-    "/~/.tapper/logs/tapper_{time}.log",
+    os.path.join(homedir, ".tapper/logs/tapper_{time}.log"),
     rotation="1 day",
     retention="3 days",
     level="TRACE",
