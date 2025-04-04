db9f3685f99455cfecfd4431ef3c546996644a90
Martin <81148675+Martian-0007@users.noreply.github.com>
2025-04-04 18:41:19 +0200

    Fix yaml loading
    Signed-off-by: Martin <81148675+Martian-0007@users.noreply.github.com>

--- src/tapper/config.py
+++ src/tapper/config.py
@@ -1,8 +1,8 @@
 from loguru import logger
-from yaml import load
+from yaml import Loader, load
 
 
 @logger.catch()
 def load_config(filepath) -> dict:
     with open(filepath, "r") as file:
-        return load(file)
+        return load(file, Loader=Loader)
