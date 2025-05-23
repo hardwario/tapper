5e9a63c97de62ed0291d7b6443294e459196e93c
Martin <81148675+Martian-0007@users.noreply.github.com>
2025-05-23 19:20:51 +0200

    Fix LED off request
    Signed-off-by: Martin <81148675+Martian-0007@users.noreply.github.com>

--- src/tapper/_outputs.py
+++ src/tapper/_outputs.py
@@ -54,7 +54,11 @@ def process_request(tapper_instance: tapper.Tapper, request_message: str) -> dic
 
         if "visual" in request:
             if "state" in request["visual"]:
-                state, color = request["visual"]["state"].split("/", 1)
+                if request["visual"]["state"] == "off":
+                    state = "off"
+                    color = None
+                else:
+                    state, color = request["visual"]["state"].split("/", 1)
 
                 logger.debug(f"Processing visual state: {state}, {color}")
 
